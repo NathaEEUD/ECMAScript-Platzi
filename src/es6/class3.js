b6d80dc7e212a1615a5c14ce47f6d31738de0078
Nathaly <nriano@robotec.com>
2020-01-31 10:55:02 -0500

    feat: parameters in objects, arrow functions and promises

---
+++ src/es6/class3.js
@@ -0,0 +1,49 @@
+/**
+ * Objects
+ */
+let name = 'Oscar';
+let age = 32;
+
+const obj = {
+  name: name,
+  age: age
+};
+console.log('Before ES6 -> ', obj);
+
+// es6
+const objES6 = { name, age };
+console.log(`After ES6 -> `, objES6);
+
+/**
+ * Arrow Functions
+ */
+const names = [
+  { name, age },
+  { name: 'Yesica', age: 27 }
+];
+
+let listOfNames = names.map(function(item) {
+  console.log('Before ES6 -> ', item.name);
+});
+
+// es6
+let listOfNamesES6 = names.map(item => console.log(`After ES6 -> ${item.name}`));
+
+/**
+ * Promises
+ */
+const helloPromise = foo => {
+  return new Promise((resolve, reject) => {
+    if (foo) {
+      resolve('Hey!');
+    } else {
+      reject('Upss!');
+    }
+  });
+};
+
+const foo = false;
+helloPromise(foo)
+  .then(response => console.log('response -> ', response))
+  .then(() => console.log('message -> Hello World!'))
+  .catch(error => console.log('error -> ', error));
